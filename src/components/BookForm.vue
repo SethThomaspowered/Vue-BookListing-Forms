a4a3663ce7e2829767262e052f67727b1b5c4290
SethThomaspowered <athomas.seth@gmail.com>
2021-10-22 21:24:49 -0400

    updating bookForm

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,7 +1,9 @@
 <template>
   <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <input v-model="bookData.bookTitle" type="text" name="title" value="" placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value="" placeholder="Book Author">
+    <input v-model="bookData.finishedReading" type="checkbox" name="finishedReading" />
+    <input v-model="bookData.ownership" type='radio'></input>    
     <button type="submit" name="button">Add Book</button>
   </form>
 </template>
@@ -12,8 +14,12 @@ export default {
   props: ["books"],
   data() {
     return {
-      bookTitle: "",
-      bookAuthor: ""
+      bookData : {
+        bookTitle: "",
+        bookAuthor: "",
+        finishedReading: false,
+        ownership: [],
+      },
     };
   },
   methods: {
